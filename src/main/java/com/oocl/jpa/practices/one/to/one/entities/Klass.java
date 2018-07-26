37a717112198b7ba88e1ecb04f6edfea2dfef570
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-27 00:47:29 +0800

    [Jeffery] add entities

---
+++ src/main/java/com/oocl/jpa/practices/one/to/one/entities/Klass.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.jpa.practices.one.to.one.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Klass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Klass() { }
+    public Klass(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public Leader getLeader() { return leader; }
+    public void setLeader(Leader leader) { this.leader = leader; }
+
+    @OneToOne(cascade = CascadeType.ALL,mappedBy = "klass",fetch = FetchType.LAZY)
+    private Leader leader = new Leader();
+}
---
+++ src/main/java/com/oocl/jpa/practices/one/to/one/entities/Leader.java
@@ -0,0 +1,50 @@
+package com.thoughtworks.jpa.practices.one.to.one.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+public class Leader {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @JsonIgnore
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "klass_id")
+    private Klass klass;
+
+    public Leader() { }
+
+    public Leader(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
