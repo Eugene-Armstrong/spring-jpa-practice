6b40ade0a5e05c8575eb1f96c26049d326953ac7
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-07-27 00:48:13 +0800

    [Jeffery] add dto

---
+++ src/main/java/com/oocl/jpa/practices/one/to/one/controllers/dto/KlassDTO.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.jpa.practices.one.to.one.controllers.dto;
+
+import com.thoughtworks.jpa.practices.one.to.one.entities.Klass;
+import com.thoughtworks.jpa.practices.one.to.one.entities.Leader;
+
+public class KlassDTO {
+    private final Long id;
+    private final String name;
+    private final Leader leader;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Leader getLeader() {
+        return leader;
+    }
+
+    public KlassDTO(Klass klass) {
+        this.id = klass.getId();
+        this.name = klass.getName();
+        this.leader = klass.getLeader();
+    }
+}
